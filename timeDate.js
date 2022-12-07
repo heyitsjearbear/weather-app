ff2a48a9be0ea45da53d27aabd35e42f809eaa81
Jeremiah Ramiscal <jereberries1215@gmail.com>
2022-12-07 10:53:50 -0800

    Simplified current time/date.

--- timeDate.js
+++ timeDate.js
@@ -1,51 +1,8 @@
-let timeElement = document.getElementById("actualTime");
 let dateElement = document.getElementById("actualDate");
-
-function getMonth(month) {
-  switch (month) {
-      case 1:
-          return "January";
-      case 2:
-          return "February";
-      case 3:
-          return "March";
-    case 4:
-      return "April";
-    case 5:
-      return "May";
-      case 6:
-          return "June";
-      case 7:
-          return "July";
-    case 8:
-      return "August";
-    case 9:
-      return "September";
-    case 10:
-      return "October";
-    case 11:
-      return "November";
-    case 12:
-      return "December";
-  }
-}
 setInterval(function () {
   let currentTime = new Date();
   let month = currentTime.getMonth();
   let day = currentTime.getDay();
   let year = currentTime.getFullYear();
-  dateElement.innerHTML = getMonth(month) + " " + day + ", " + year;
-  let hours = currentTime.getHours();
-  let minutes = currentTime.getMinutes();
-  let seconds = currentTime.getSeconds();
-  if (minutes < 10) {
-    minutes = "0" + minutes;
-  }
-  let time = hours + ":" + minutes + ":" + seconds + " ";
-  if (hours > 11) {
-    time += "PM";
-  } else {
-    time += "AM";
-  }
-  timeElement.innerHTML = "Current Local Time: " + time;
+  dateElement.innerHTML = currentTime;
 }, 1000);
