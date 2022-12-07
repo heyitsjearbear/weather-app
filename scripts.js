bc3ddbce8c1fc25520d28a2f99b3bd4dc56783cd
Jeremiah Desktop <jereberries1215@gmail.com>
2022-12-06 16:36:51 -0800

    Got local date to work.

--- scripts.js
+++ scripts.js
@@ -1,17 +1,51 @@
-var timeElement = document.getElementById('actualTime');
-setInterval(function() {
-    var currentTime = new Date();
-    var hours = currentTime.getHours();
-    var minutes = currentTime.getMinutes();
-    var seconds = currentTime.getSeconds();
-    if (minutes < 10){
-        minutes = "0" + minutes
-    }
-    var time = hours + ":" + minutes + ":" + seconds + " ";
-    if(hours > 11){
-        time += "PM";
-    } else {
-        time += "AM";
-    }
-    timeElement.innerHTML = "Current Local Time" + time;
+function getMonth(month) {
+  switch (month) {
+      case 1:
+          return "January";
+      case 2:
+          return "February";
+      case 3:
+          return "March";
+    case 4:
+      return "April";
+    case 5:
+      return "May";
+      case 6:
+          return "June";
+      case 7:
+          return "July";
+    case 8:
+      return "August";
+    case 9:
+      return "September";
+    case 10:
+      return "October";
+    case 11:
+      return "November";
+    case 12:
+      return "December";
+  }
+}
+
+let timeElement = document.getElementById("actualTime");
+let dateElement = document.getElementById("actualDate");
+setInterval(function () {
+  let currentTime = new Date();
+  let month = currentTime.getMonth();
+  let day = currentTime.getDay();
+  let year = currentTime.getFullYear();
+  dateElement.innerHTML = getMonth(month) + " " + day + ", " + year;
+  let hours = currentTime.getHours();
+  let minutes = currentTime.getMinutes();
+  let seconds = currentTime.getSeconds();
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+  let time = hours + ":" + minutes + ":" + seconds + " ";
+  if (hours > 11) {
+    time += "PM";
+  } else {
+    time += "AM";
+  }
+  timeElement.innerHTML = "Current Local Time: " + time;
 }, 1000);
