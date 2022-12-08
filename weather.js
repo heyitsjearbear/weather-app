c4eb050e9813714b3d22d4dd94eb48172eecd702
Jeremiah Ramiscal <jereberries1215@gmail.com>
2022-12-07 18:44:21 -0800

    Got weather to work on submit button

---
+++ weather.js
@@ -0,0 +1,23 @@
+let submit = document.querySelector("#submit");
+let temperature = document.querySelector(".degrees-celsius");
+submit.addEventListener("click", () => {
+  let city = document.querySelector("#city-name").value;
+  console.log(`Entered ${city}`);
+  fetch(
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+      city +
+      "&appid=98c13a7725d224f508da1c6f66e1423a"
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      var tempValue = data["main"]["temp"];
+      var nameValue = data["name"];
+      var descValue = data["weather"][0]["description"];
+
+      temperature.innerHTML = tempValue;
+      city = "";
+    })
+
+    .catch((err) => alert("Wrong city name!"));
+});
