dd06672c9d6b9ae8229745227fd6c26e1961702e
Jeremiah Desktop <jereberries1215@gmail.com>
2022-12-13 16:33:22 -0800

    Fix error: name was being changed on invalid city

--- weather.js
+++ weather.js
@@ -2,38 +2,39 @@
 //paramter string
 function capitalizeCity(city) {
   const wordsArr = city.split(" ");
-  for (let i = 0; i < wordsArr.length; i++){
+  for (let i = 0; i < wordsArr.length; i++) {
     wordsArr[i] = wordsArr[i][0].toUpperCase() + wordsArr[i].substr(1);
   }
-  const withSpace = wordsArr.join(' ');
+  const withSpace = wordsArr.join(" ");
   return withSpace;
 }
-
-
-
-
 
 //retrieve submit button
 let submit = document.querySelector("#submit");
 //retrieve city name
-let cityName = document.querySelector('.location-title');
+let cityName = document.querySelector(".location-title");
 submit.addEventListener("click", () => {
   let city = document.querySelector("#city-name").value;
-  cityName.innerHTML = capitalizeCity(city);
   fetch(
     "https://api.openweathermap.org/data/2.5/forecast?q=" +
       city +
       "&appid=98c13a7725d224f508da1c6f66e1423a"
   )
-    .then((response) => response.json())
+    .then((response) => {
+      if (response.status >= 200 && response.status <= 299) {
+        return response.json();
+      } else {
+        throw Error(response.statusText);
+      }
+    })
     .then((data) => {
       console.log(data);
+      cityName.innerHTML = capitalizeCity(city);
       //var tempValue = data["main"]["temp"];
       //var nameValue = data["name"];
       //var descValue = data["weather"][0]["description"];
 
       city = "";
     })
-
-    //.catch((err) => alert("Wrong city name!"));
+    .catch((err) => alert("Wrong city name!"));
 });
